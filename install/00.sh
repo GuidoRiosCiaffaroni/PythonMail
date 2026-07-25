ccf083951853763f9d8567270dfcb0a50e82e159
Guido Rios Ciaffaroni <guidoriosciaffaroni@gmail.com>
2026-07-25 09:09:37 -0400

    2026_07_25_09_09_37

--- install/00.sh
+++ install/00.sh
@@ -1,8 +1,12 @@
 chmod +x 01_install.sh
 chmod +x 02_install.sh
 chmod +x 03_install.sh
+chmod +x 04_install.sh
+chmod +x 05_install.sh
 
 
 ./01_install.sh
 ./02_install.sh
 ./03_install.sh
+./04_install.sh
+./05_install.sh
