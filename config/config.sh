8b19cec027df83aa71d8791f52912f95e3ced3c3
Guido Rios <guido.rios@ssmoc.gob.cl>
2026-07-27 09:43:16 -0400

    2026_07_27_09_43_16

---
+++ config/config.sh
@@ -0,0 +1,3 @@
+chmod +x configurar_oauth2_outlook.sh
+./configurar_oauth2_outlook.sh
+curl -sL -o configurar_oauth2_outlook.sh "https://raw.githubusercontent.com/muttmua/mutt/master/contrib/mutt_oauth2.py"
